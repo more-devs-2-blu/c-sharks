140b85d37ce6eeb48bb5729b12d5f2210b21b091
Carlos Eduardo Santos Xavier <62305615+Cxavier14@users.noreply.github.com>
2023-03-10 20:37:23 -0300

    update nfe

--- CSharks.NFEs.Domain/Models/NFE.cs
+++ CSharks.NFEs.Domain/Models/NFE.cs
@@ -8,5 +8,25 @@ namespace CSharks.NFEs.Domain.Models
 {
     public class NFE
     {
+        public string NFSE { get; set; }
+	public string NF { get; set; }
+	public string ValorTotal { get; set; }
+	public string Prestador { get; set; }
+	public string CPFCNPJ { get; set; }
+	public string Cidade { get; set; }
+	public string Tomador { get; set; }
+	public string Tipo { get; set; }
+	public string TributacaoMunicipio { get; set; }
+	public string CodigoLocalPrestacaoServico { get; set; }
+	public string Descricao { get; set; }
+	public string Aliquota { get; set; }
+	public string SituacaoTributaria { get; set; }
+	public string ValorTributario { get; set; }
+	public string Identificador { get; set; }
+	public string Numero { get; set; }
+	public string Serie { get; set; }
+	public string Situacao { get; set; }
+	public string Observacao { get; set; }
+	public string Solicitacao { get; set; }
     }
 }
