7aa4c551e6f62d00144a58d3055022e0a361e382
Carlos Eduardo Santos Xavier <62305615+Cxavier14@users.noreply.github.com>
2023-03-10 20:38:05 -0300

    update service

--- CSharks.NFEs.Domain/Models/Service.cs
+++ CSharks.NFEs.Domain/Models/Service.cs
@@ -8,5 +8,11 @@ namespace CSharks.NFEs.Domain.Models
 {
     public class Service
     {
+        public int Id { get; set; }
+	public string Name { get; set; }
+	public double Aliquot { get; set; }
+	
+	public int CategoryId { get; set; }
+	public virtual Category? Category { get; set; }
     }
 }
