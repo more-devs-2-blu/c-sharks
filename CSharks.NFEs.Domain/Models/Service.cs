bc1ca0757b2d893e94c4a0385dd4cc9770f8aefc
Carlos Eduardo Santos Xavier <62305615+Cxavier14@users.noreply.github.com>
2023-03-10 20:44:09 -0300

    update service

--- CSharks.NFEs.Domain/Models/Service.cs
+++ CSharks.NFEs.Domain/Models/Service.cs
@@ -8,7 +8,7 @@ namespace CSharks.NFEs.Domain.Models
 {
     public class Service
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; }
 	public string Name { get; set; }
 	public double Aliquot { get; set; }
 	
