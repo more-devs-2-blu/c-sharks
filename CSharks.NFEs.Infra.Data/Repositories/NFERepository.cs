b5dcac98ea8aa91de665b9eecfdde2a0290f55f2
Alexandre Schwanke <alexandreschwc@gmail.com>
2023-03-11 14:37:47 -0300

    fix: implementaçao do base repository nos outros repositories

--- CSharks.NFEs.Infra.Data/Repositories/NFERepository.cs
+++ CSharks.NFEs.Infra.Data/Repositories/NFERepository.cs
@@ -5,37 +5,11 @@ using CSharks.NFEs.Infra.Data.Data;
 namespace CSharks.NFEs.Infra.Data.Repositories
 {
 
-    public class NFERepository : INFERepository
+    public class NFERepository : BaseRepository<NFE>, INFERepository
     {
-        private readonly MySqlContext _dataContext;
-        public NFERepository(MySqlContext context)
+        private readonly MySqlContext? _dataContext;
+        public NFERepository(MySqlContext context) : base(context)
         {
-            _dataContext = context;
-        }
-
-        public Task<int> Delete(NFE entity)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IQueryable<NFE> GetAll()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<NFE> GetById(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<int> Save(NFE entity)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<int> Update(NFE entity)
-        {
-            throw new NotImplementedException();
         }
     }
 }
