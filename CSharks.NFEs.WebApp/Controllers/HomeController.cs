fa117b5de3aa303a73b9767529584e2eaa332638
juninhocb <juninhocb@homail.com>
2023-03-11 18:53:55 -0300

    fix and feat: session, now working

--- CSharks.NFEs.WebApp/Controllers/HomeController.cs
+++ CSharks.NFEs.WebApp/Controllers/HomeController.cs
@@ -1,13 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CSharks.NFEs.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace CSharks.NFEs.WebApp.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ISessionService _session;
+
+        public HomeController(ISessionService  session)
+        {
+            _session = session;
+        }
+
 
         public IActionResult Index()
         {
+            TempData["NameUserLogged"] = _session.GetSession().Name;
             return View();
         }
     }
