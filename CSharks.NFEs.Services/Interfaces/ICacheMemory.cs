74652a50e2048a4c682da8e7e27d5bf32170f460
juninhocb <juninhocb@homail.com>
2023-03-13 16:34:08 -0300

    feat: caching data to improve performance

---
+++ CSharks.NFEs.Services/Interfaces/ICacheMemory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharks.NFEs.Services.Interfaces
+{
+    public interface ICacheMemory
+    {
+        T Get<T>(string key);
+        void Set<T>(string key, T value, TimeSpan expirationTime);
+        void Remove(string key);
+    }
+}
--- CSharks.NFEs.WebApp/Controllers/ServiceController.cs
+++ CSharks.NFEs.WebApp/Controllers/ServiceController.cs
@@ -1,6 +1,8 @@
 ﻿using CSharks.NFEs.Domain.Interfaces.Repositories;
 using CSharks.NFEs.Domain.Models;
+using CSharks.NFEs.Domain.Models.NF_UTILS;
 using CSharks.NFEs.Infra.Data.Repositories;
+using CSharks.NFEs.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CSharks.NFEs.WebApp.Controllers
@@ -9,36 +11,47 @@ namespace CSharks.NFEs.WebApp.Controllers
     {
         private readonly IServicesRepository _serviceRepo;
         private readonly IIsqnRepository _isqnRepo;
+        private readonly ICacheMemory _memoryCache; 
+
 
         public ServiceController(
-            IServicesRepository servicesRepository, IIsqnRepository isqnRepository)
+            IServicesRepository servicesRepository, IIsqnRepository isqnRepository, ICacheMemory cacheMemory)
         {
             _serviceRepo = servicesRepository;
             _isqnRepo = isqnRepository;
+            _memoryCache = cacheMemory;
         }
         public IActionResult Index()
         {
-            //ViewBag.services = _serviceRepo.GetAll().ToList();
-            ViewBag.services = _isqnRepo.GetAll().ToList();
+            ViewBag.isqns = GetISQNS();
             return View("~/Views/Register/Services/Index.cshtml");
         }
 
         [HttpPost]
         public IActionResult Create(Service service)
         {
+            ViewBag.isqns = GetISQNS();
             if (ModelState.IsValid)
             {
                 _serviceRepo.Save(service); 
                 TempData["Success"] = "Salvo com sucesso";
-                return View("Index");
-            }
-            else
-            {
                 return View("~/Views/Register/Services/Index.cshtml");
             }
 
-
             return View("~/Views/Register/Services/Index.cshtml");
+        }
+
+        private List<ISQN> GetISQNS()
+        {
+            List<ISQN> isqns = _memoryCache.Get<List<ISQN>>("ISQN_LIST");
+
+            if (isqns == null)
+            {
+                isqns = _isqnRepo.GetAll().ToList();
+                _memoryCache.Set("ISQN_LIST", isqns, TimeSpan.FromMinutes(45));
+            }
+
+            return isqns;
         }
     }
 }
