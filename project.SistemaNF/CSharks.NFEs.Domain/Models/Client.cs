d7671d1438bde4d27b3ccd7c83abda526cdf6b5d
patrickweber <patrickrenanweber@gmail.com>
2023-03-14 20:00:42 -0300

    feat: mapeamento entidade

--- project.SistemaNF/CSharks.NFEs.Domain/Models/Client.cs
+++ project.SistemaNF/CSharks.NFEs.Domain/Models/Client.cs
@@ -9,6 +9,14 @@ namespace CSharks.NFEs.Domain.Models
 		public string Name { get; set; }
         [Required(ErrorMessage = "Digite o cpf ou cnpj do cliente")]
         public string CpfCnpj { get; set; }
+        public string Type { get; set; }
+        public string Address { get; set; }
+        public string Identifier { get; set; }
+        public string Ie { get; set; }
+        public string Patio { get; set; }
+        public string Email { get; set; }
+        public string ResidentialNumber { get; set; }
+        public string Complement { get; set; }
 
         public string Enrollment { get; set; }
 	
