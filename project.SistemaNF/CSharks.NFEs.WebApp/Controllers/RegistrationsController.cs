c95b33375960426016d9e748155fd4a9409246bc
Eduardo Xavier <eduardoxavi15@gmail.com>
2023-03-14 22:45:51 -0300

    update registration

---
+++ project.SistemaNF/CSharks.NFEs.WebApp/Controllers/RegistrationsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CSharks.NFEs.WebApp.Controllers
+{
+    public class RegistrationsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult RegisterService()
+        {
+            return View("~/Views/Register/Services/Index.cshtml");
+        }
+
+        public IActionResult RegisterUser()
+        {
+            return View("~/Views/Register/Users/Index.cshtml");
+        }
+
+        public IActionResult RegisterClient()
+        {
+            return View("~/Views/Register/Clients/Index.cshtml");
+        }
+    }
+}
