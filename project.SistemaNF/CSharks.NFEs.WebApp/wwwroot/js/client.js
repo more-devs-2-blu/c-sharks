1d57a66cbd5462ab824cd887da4f4ccea920f23c
Alexandre Schwanke da Costa <alexandreschwc@gmail.com>
2023-03-15 14:13:36 -0300

    feat: integração viaCEP an hora de cadastrar cliente

---
+++ project.SistemaNF/CSharks.NFEs.WebApp/wwwroot/js/client.js
@@ -0,0 +1,19 @@
+﻿$('document').ready(e => {
+    $('#input-cep').keyup(e => {
+        let valueLength = e.target.value.length;
+        if (valueLength == 8) {
+            let cep = e.target.value;
+            $.ajax({
+                url: `https://viacep.com.br/ws/${cep}/json/`,
+                method: 'GET',
+                success: res => {
+                    console.log(res);
+                    $(`#input-city`).val(res.localidade);
+                    $('#input-district').val(res.bairro);
+                    $('#input-address').val(res.logradouro);
+                    $('#hidden-input-city').val(res.siafi);
+                }
+            })
+        }
+    })
+})
